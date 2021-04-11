c73e5f475bd656971c92733931d8138e5862067e
Minh Nguyen <mnguyen728@hotmail.com>
2021-04-11 00:09:58 -0700

    keys objs updated

--- assets/script/script.js
+++ assets/script/script.js
@@ -4,32 +4,47 @@ var startBtn = $("#start-button");
 var score = $('#score');
 /// Questions and Answers
 const questions = [ {question: "What is an API?",
-                    answerOne: "Access Portal Interface",
-                    answertwo: "Application Program Interface",
-                    answerThree: "Application Point Interlace",
-                    answerFour: "Artificial Progamming Imprint",
-                    correctAnswer: "2"},
+                    answerOne:
+                     {quest:"Access Portal Interface",
+                      correctAnswer: true},
+                    answertwo: {quest:"Application Program Interface",
+                                correctAnswer:false},
+                    answerThree: {quest:"Application Point Interlace",
+                                correctAnswer:false},
+                    answerFour: {quest:"Artificial Progamming Imprint",
+                                correctAnswer: false},
+                    },
 
                     {question: "What does the DOM stand for?",
-                    answerOne: "Digital Object Modal",
-                    answertwo: "Data Oriented Module",
-                    answerThree: "Document Object Model",
-                    answerFour: "Desktop Object Monitor",
-                    correctAnswer: "3"},
+                    answerOne: {quest:"Digital Object Modal",
+                                correctAnswer:false},
+                    answertwo: {quest:"Data Oriented Module",
+                                correctAnswer: false},
+                    answerThree: {quest:"Document Object Model",
+                                correctAnswer:true},
+                    answerFour: { quest:"Desktop Object Monitor",
+                                correctAnswer: false}},
 
                     {question: "What file is use mainly to style the website?",
-                    answerOne: "CSS",
-                    answertwo: "Javascript",
-                    answerThree: "Dom",
-                    answerFour: "Bootsrap",
-                    correctAnswer: "1"},
+                    answerOne: {quest:"CSS",
+                                correctAnswer: true},
+                    answertwo: {quest:"Javascript",
+                                correctAnswer:false},
+                    answerThree: {quest:"Dom",
+                                correctAnswer:false},
+                    answerFour: {quest:"Bootsrap",
+                                correctAnswer: false}},
 
-                    {question: "What file is use mainly to style the website?",
-                    answerOne: "CSS",
-                    answertwo: "Javascript",
-                    answerThree: "Dom",
-                    answerFour: "Bootsrap",
-                    correctAnswer: "1"}
+
+                    {question: "What file is use mainly to add function the website?",
+                    answerOne: {quest:"CSS",
+                                correctAnswer:false},
+                    answertwo: {quest:"Javascript",
+                                correctAnswer: true},
+                    answerThree: {quest:"Dom",
+                                correctAnswer:false},
+                    answerFour: {quest:"Bootsrap",
+                                correctAnswer:false}}
 
                     ];
 
@@ -60,11 +75,33 @@ function timerCountdown(){
 function questionGenerator(){
     var activeQuest = questions[activeQuestNum];
     $('#test-question').text(activeQuest.question);
-    $('#button-a').html(activeQuest.answerOne);
-    $('#button-b').html(activeQuest.answertwo);
-    $('#button-c').html(activeQuest.answerThree);
-    $('#button-d').html(activeQuest.answerFour);
+    $('#button-a').html(activeQuest.answerOne.quest);
+    $('#button-b').html(activeQuest.answertwo.quest);
+    $('#button-c').html(activeQuest.answerThree.quest);
+    $('#button-d').html(activeQuest.answerFour.quest);
 }
+
+
+
+function checkAnswer(answer){
+    correctAnswer = questions;
+
+    if (answer === correct && currentQuestionIndex !== finalQuestionIndex){
+        score++;
+        alert("That Is Correct!");
+        currentQuestionIndex++;
+        generateQuizQuestion();
+        //display in the results div that the answer is correct.
+    }else if (answer !== correct && currentQuestionIndex !== finalQuestionIndex){
+        alert("That Is Incorrect.")
+        currentQuestionIndex++;
+        generateQuizQuestion();
+        //display in the results div that the answer is wrong.
+    }else{
+        showScore();
+    }
+}
+
 
 
 //Start the quiz main function
